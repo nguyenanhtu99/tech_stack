a1f4f6b31b338df63c7c40f1c83f876261da6a4e
Tu Nguyen <nguyenanhtu1999@gmail.com>
2021-05-18 14:39:42 +0300

    adding touchable

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListItem } from 'react-native-elements';
 import * as actions from '../actions';
+import { TouchableOpacity } from 'react-native';
 
 class RenderItem extends Component {
     render() {
-        console.log(this.props.library)
+        const { id, title } = this.props.library.item;
         return (
-            <ListItem>
-                <ListItem.Content>
-                    <ListItem.Title>{this.props.library.item.title}</ListItem.Title>
-                </ListItem.Content>
-                <ListItem.Chevron/>
-            </ListItem>
+            <TouchableOpacity
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <ListItem>
+                    <ListItem.Content>
+                        <ListItem.Title>{title}</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Chevron/>
+                </ListItem>
+            </TouchableOpacity>
         )
     }
 }
